5caad25944b3333045a1c6309b17732d694ef9be
MeyTeplon <meylani@students.amikom.ac.id>
2025-07-29 20:41:42 +0700

    ready deploy

--- main.py
+++ main.py
@@ -14,14 +14,14 @@ app = FastAPI(
 )
 
 # --- Konfigurasi CORS ---
-origins = [
-    "http://localhost",
-    "http://localhost:3000",
-]
+# origins = [
+#     "http://localhost",
+#     "http://localhost:3000",
+# ]
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=["*"],  # Allow all origins for development; adjust in production
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
